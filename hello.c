8fa43b83a0258123aa3337c6e962997033b6f9dd
Bryce Yong <byong3@huskers.unl.edu>
2021-08-25 15:53:03 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+2 * Author: Bryce Yong
+3 * Date: 2021/8/23
+4 *
+5 * A simple hello world program in C
+6 *
+7 */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World!\n");
+    printf("Computer Science Major"\n");
+
+    return 0;
+
+}
